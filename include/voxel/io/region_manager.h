5ff7be56aaf7265aac340c499ae6b3080b70f38a
Matthew Marshall <matthew.marshall090396@gmail.com>
2022-06-16 22:53:08 +0100

    Skeleton for beginning IO for blocks and chunks.

---
+++ include/voxel/io/region_manager.h
@@ -0,0 +1,11 @@
+#ifndef __hemlock_voxel_io_region_manager_h
+#define __hemlock_voxel_io_region_manager_h
+
+namespace hemlock {
+    namespace voxel {
+        
+    }
+}
+namespace hvox = hemlock::voxel;
+
+#endif // __hemlock_voxel_io_region_manager_h
